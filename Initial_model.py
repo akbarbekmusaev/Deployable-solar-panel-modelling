e71241df8f2219578f9fbdf8c268429d2372f460
akbarbekmusaev <162443882+akbarbekmusaev@users.noreply.github.com>
2024-03-22 22:42:50 +0000

    created spring and damper functions that model  it

--- Initial_model.py
+++ Initial_model.py
@@ -1,9 +1,7 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-from Pendulum_Animations import animate_pendulum
-from matplotlib.animation import PillowWriter
-import webbrowser
+import simpy as sp
 
 # constants
 T = 100
@@ -28,8 +26,83 @@ M_total = M_panels + 2 * (M_driving + M_green + M_frame + M_blue)
 
 # Define function to calculate phi
 def phi(theta):
-    phi = theta + np.arcsin(L_base / L_shortbeam)
-    return phi
+    return theta + np.arcsin(L_base / L_shortbeam)
+
+def dampertorque(c, theta, omega):
+    #End-point
+    H = 0.5 * L_longbeam * np.cos(theta) + 0.5 * L_shortbeam * np.cos(phi(theta) - theta)
+    V = 0.5 * L_longbeam * np.sin(theta) - 0.5 * L_shortbeam * np.sin(phi(theta) - theta)
+    R = np.sqrt(H ** 2 + V ** 2)
+    #Blue beam
+    R_blue = np.sqrt(L_shortbeam ** 2 + (0.5 * L_longbeam) ** 2 - L_shortbeam ** 2 * np.cos(np.pi - phi(theta)))
+    H_blue = np.sqrt(L_shortbeam ** 2 - L_base ** 2) + 0.5 * L_shortbeam * np.cos(theta)
+    V_blue = np.sqrt(R_blue ** 2 - H_blue ** 2)
+    #Green beam
+    R_green = np.sqrt(
+        (0.5 * L_longbeam) ** 2 + (0.25 * L_longbeam) ** 2 - 0.25 * L_longbeam ** 2 * np.cos(np.pi - phi(theta)))
+    H_green = 0.5 * L_longbeam * np.cos(theta) + 0.25 * L_longbeam * np.cos(phi(theta) - theta)
+    V_green = np.sqrt(R_green ** 2 - H_green ** 2)
+    #Angle a
+    H_a = H - H_blue
+    V_a = V - V_blue
+    R_a = np.sqrt(H_a ** 2 + V_a ** 2)
+    angle_a = np.arccos(((-H_a)*H + (-V_a)*V) / (R_a * R))
+    #angle b
+    H_b = H - H_green
+    V_b = V - V_green
+    R_b = np.sqrt(H_b ** 2 + V_b ** 2)
+    angle_b = np.arccos(((-H_b) * H + (-V_b) * V) / (R_b * R + 1e-10))
+
+    #Calculate speed and torque
+    H_prime = -0.5 * omega * L_longbeam * np.sin(theta)
+    V_prime = 0.5 * omega * L_longbeam * np.cos(theta)
+    speed = np.sqrt(H_prime ** 2 + V_prime ** 2)
+    F_magnitude = c * speed
+
+    F_green = F_magnitude * (np.sin(angle_b)/np.sin(angle_a + angle_b))
+    Torque = F_green * L_shortbeam * np.cos(phi(theta) - np.pi/2)
+
+    return Torque
+
+def springtorque(k, theta, theta_initial):
+    #initial position of the spring
+    H_initial = 0.5 * L_longbeam * np.cos(theta_initial) + 0.5 * L_shortbeam * np.cos(
+        phi(theta_initial) - theta_initial)
+    V_initial = 0.5 * L_longbeam * np.sin(theta_initial) - 0.5 * L_shortbeam * np.sin(
+        phi(theta_initial) - theta_initial)
+    R_initial = np.sqrt(H_initial ** 2 + V_initial ** 2)
+    # End-point
+    H = 0.5 * L_longbeam * np.cos(theta) + 0.5 * L_shortbeam * np.cos(phi(theta) - theta)
+    V = 0.5 * L_longbeam * np.sin(theta) - 0.5 * L_shortbeam * np.sin(phi(theta) - theta)
+    R = np.sqrt(H ** 2 + V ** 2)
+    # Blue beam
+    R_blue = np.sqrt(L_shortbeam ** 2 + (0.5 * L_longbeam) ** 2 - L_shortbeam ** 2 * np.cos(np.pi - phi(theta)))
+    H_blue = np.sqrt(L_shortbeam ** 2 - L_base ** 2) + 0.5 * L_shortbeam * np.cos(theta)
+    V_blue = np.sqrt(R_blue ** 2 - H_blue ** 2)
+    # Green beam
+    R_green = np.sqrt(
+        (0.5 * L_longbeam) ** 2 + (0.25 * L_longbeam) ** 2 - 0.25 * L_longbeam ** 2 * np.cos(np.pi - phi(theta)))
+    H_green = 0.5 * L_longbeam * np.cos(theta) + 0.25 * L_longbeam * np.cos(phi(theta) - theta)
+    V_green = np.sqrt(R_green ** 2 - H_green ** 2)
+    # Angle a
+    H_a = H - H_blue
+    V_a = V - V_blue
+    R_a = np.sqrt(H_a ** 2 + V_a ** 2)
+    angle_a = np.arccos(((-H_a) * H + (-V_a) * V) / (R_a * R))
+    # angle b
+    H_b = H - H_green
+    V_b = V - V_green
+    R_b = np.sqrt(H_b ** 2 + V_b ** 2)
+    angle_b = np.arccos(((-H_b) * H + (-V_b) * V) / (R_b * R + 1e-10))
+
+    # Calculate displacement and torque
+    displacement = np.sqrt((H - H_initial) ** 2 + (V - V_initial) ** 2)
+    F_magnitude = k * displacement
+
+    F_green = F_magnitude * (np.sin(angle_b) / np.sin(angle_a + angle_b))
+    Torque = F_green * L_shortbeam * np.cos(phi(theta) - np.pi / 2)
+
+    return Torque
 
 
 def centre_of_mass(theta):
@@ -102,7 +175,8 @@ def plot_com_change():
     plt.show()
 
 
-def openingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_max, gearRatio, c):
+def openingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_max, gearRatio, c, k):
+
     # Define function to calculate output torque from gearbox to mechanism
     def torque_out(speed):
         torque = torque_in(speed) * gearRatio
@@ -118,8 +192,7 @@ def openingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_m
     # Define function for differential equation
     def opening(t, z):
         difz = [z[1],
-                (torque_out(z[1]) / (M_total * centre_of_mass(z[0]) ** 2)) - ((g * np.cos(z[0])) / centre_of_mass(z[0])) - ((c * z[1]) / (
-                            M_total * centre_of_mass(z[0]) ** 2))]
+                (torque_out(z[1]) / (M_total * centre_of_mass(z[0]) ** 2)) - ((g * np.cos(z[0])) / centre_of_mass(z[0])) - (dampertorque(c, z[0], z[1])/(M_total*centre_of_mass(z[0]) ** 2)) + (springtorque(k, z[0], theta_initial) / (M_total*centre_of_mass(z[0]) ** 2))]
         return difz
 
     # Define event to stop the simulation when the pendulum reaches the finishing angle
@@ -149,7 +222,7 @@ def closingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_m
     # Define function for differential equation
     def closing(t, z):
         difz = [z[1],
-                (-torque_out(z[1]) / (M_total * centre_of_mass(z[0]) ** 2)) - (g * np.cos(z[0])) / centre_of_mass(z[0]) - (c * z[1])/(M_total * centre_of_mass(z[0]) ** 2)]
+                (-torque_out(z[1]) / (M_total * centre_of_mass(z[0]) ** 2)) - (g * np.cos(z[0])) / centre_of_mass(z[0]) + (dampertorque(c, z[0], z[1]) / (M_total*centre_of_mass(z[0]) ** 2))]
         return difz
 
     # Define event to stop the simulation when the pendulum reaches the finishing angle
--- damping analysis.py
+++ damping analysis.py
@@ -1,34 +1,36 @@
 from Initial_model import openingmodel
 from Initial_model import closingmodel
+from Initial_model import dampertorque
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Constants
-theta_finishing = np.pi / 2
-theta_initial = np.pi / 18
+theta_f = 90 * (np.pi / 180)
+theta_i = np.pi / 12
 speed_initial = 0
 T_stall = 0.69
 omega_max = 3700
 gear_ratio = 270
-c = list(range(0, 400, 20))  # Damping coefficient
+c_values = [100]
+k = 50
 
 def rad_s_to_rpm(rad_s):
     return (rad_s * 60) / (2 * np.pi)
 
 # Define function to plot time and position of model
-def TimeAndPositionOfModelClosing_plot(theta_finishing, c_values):
+def TimeAndPositionOfModelClosing_plot(theta_initial, theta_finishing, c_values):
     fig, axs = plt.subplots(2, figsize=(10, 10))
     axs[0].set_ylabel('Angular displacement (rad)')
     axs[0].set_xlabel('Time')
     axs[0].set_title('Angular displacement of the model over Time')
     axs[1].set_xlabel('Time')
-    axs[1].set_ylabel('Angular velocity (RPM)')
+    axs[1].set_ylabel('Angular velocity')
     axs[1].set_title('Angular velocity of the model over Time')
 
     for c in c_values:
-        sol = closingmodel(theta_finishing, theta_initial, speed_initial, T_stall, omega_max, gear_ratio, c)
+        sol = closingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_max, gear_ratio, c)
         axs[0].plot(sol.t, sol.y[0, :], label='c = {}'.format(c))
-        axs[1].plot(sol.t, rad_s_to_rpm(sol.y[1, :]), label='c = {}'.format(c))
+        axs[1].plot(sol.t, sol.y[1, :], label='c = {}'.format(c))
 
     axs[0].axhline(y=theta_finishing, color='r', linestyle='--', label='Finishing Theta')
     axs[0].legend()
@@ -36,19 +38,19 @@ def TimeAndPositionOfModelClosing_plot(theta_finishing, c_values):
     plt.subplots_adjust(hspace=0.5)
     plt.show()
 
-def TimeAndPositionOfModelOpening_plot(theta_finishing, c_values):
+def TimeAndPositionOfModelOpening_plot(theta_initial, theta_finishing, c_values):
     fig, axs = plt.subplots(2, figsize=(10, 10))
     axs[0].set_ylabel('Angular displacement (rad)')
     axs[0].set_xlabel('Time')
     axs[0].set_title('Angular displacement of the model over Time')
     axs[1].set_xlabel('Time')
-    axs[1].set_ylabel('Angular velocity (RPM)')
+    axs[1].set_ylabel('Angular velocity')
     axs[1].set_title('Angular velocity of the model over Time')
 
     for c in c_values:
-        sol = openingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_max, gear_ratio, c)
+        sol = openingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_max, gear_ratio, c, k)
         axs[0].plot(sol.t, sol.y[0, :], label='c = {}'.format(c))
-        axs[1].plot(sol.t, rad_s_to_rpm(sol.y[1, :]), label='c = {}'.format(c))
+        axs[1].plot(sol.t, sol.y[1, :], label='c = {}'.format(c))
 
     axs[0].axhline(y=theta_finishing, color='r', linestyle='--', label='Finishing Theta')
     axs[0].legend()
@@ -56,8 +58,22 @@ def TimeAndPositionOfModelOpening_plot(theta_finishing, c_values):
     plt.subplots_adjust(hspace=0.5)
     plt.show()
 
-# Main code
-# sol = openingmodel(theta_initial, theta_finishing, speed_initial, T_stall, omega_max,
-#                    gear_ratio, c)
-TimeAndPositionOfModelClosing_plot(theta_finishing, c)
-TimeAndPositionOfModelOpening_plot(theta_finishing, c)
+TimeAndPositionOfModelOpening_plot(theta_i, theta_f, c_values)
+#TimeAndPositionOfModelClosing_plot(theta_f, theta_i, c_values)
+# Run the opening model
+sol = closingmodel(theta_f, theta_i, speed_initial, T_stall, omega_max, gear_ratio, c_values[0])
+
+# Extract theta and speed values from the solution
+theta_values = sol.y[0]
+speed_values = sol.y[1]
+
+# Calculate the damper torque for each pair of theta and speed values
+torque_values = [dampertorque(c_values[0], theta, speed) for theta, speed in zip(theta_values, speed_values)]
+
+# Plot damper torque vs theta
+plt.figure()
+plt.plot(theta_values, torque_values)
+plt.xlabel('Theta (rad)')
+plt.ylabel('Damper Torque (Nm)')
+plt.title('Damper Torque vs Theta')
+#plt.show()
