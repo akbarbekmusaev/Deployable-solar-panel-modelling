bd98b7edcb271ef9cd87f37702ab9b56fab9ad29
akbarbekmusaev <162443882+akbarbekmusaev@users.noreply.github.com>
2024-04-24 08:59:04 +0100

    graphs

--- damping analysis.py
+++ damping analysis.py
@@ -10,8 +10,8 @@ speed_initial = 0
 T_stall = 0.69
 omega_max = 3700
 gear_ratio = 386.7
-c_values_opening = np.arange(0, 1000, 100)
-c_values_closing = np.arange(0, 3000, 100)
+c_values_opening = [950]
+c_values_closing = [2300]
 efficiency = 0.73
 
 #08-020
@@ -113,7 +113,7 @@ def closing_analysis(c_values, theta_initial, theta_finishing, speed_initial, T_
     plt.show()
 
 
-#TimeAndPositionOfModelOpening_plot_c(theta_i, theta_f, c_values_opening, k_start, k_finish)
-#TimeAndPositionOfModelClosing_plot_c(theta_f, theta_i, c_values_closing, k_start, k_finish)
+TimeAndPositionOfModelOpening_plot_c(theta_i, theta_f, c_values_opening, k_start, k_finish)
+TimeAndPositionOfModelClosing_plot_c(theta_f, theta_i, c_values_closing, k_start, k_finish)
 #opening_analysis(c_values_opening, theta_i, theta_f, speed_initial, T_stall, omega_max, gear_ratio, k_start, k_finish, efficiency)
-closing_analysis(c_values_closing, theta_f, theta_i, speed_initial, T_stall, omega_max, gear_ratio, k_start, k_finish, efficiency)
+#closing_analysis(c_values_closing, theta_f, theta_i, speed_initial, T_stall, omega_max, gear_ratio, k_start, k_finish, efficiency)
